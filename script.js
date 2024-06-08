96748260bf784820258d240f2b31316a584be751
tithika-char <141701432+tithika-char@users.noreply.github.com>
2024-06-09 01:46:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
 let timer;
+let isRunning = false;
 let time = 0;
 
 function startTimer() {
-    if (!timer) {
-        timer = setInterval(updateTimer, 1000);
+    if (!isRunning) {
+        isRunning = true;
+        timer = setInterval(() => {
+            time++;
+            document.getElementById('timer').querySelector('h1').textContent = new Date(time * 1000).toISOString().substr(11, 8);
+        }, 1000);
     }
 }
 
 function stopTimer() {
+    isRunning = false;
     clearInterval(timer);
-    timer = null;
 }
 
 function resetTimer() {
-    stopTimer();
+    isRunning = false;
+    clearInterval(timer);
     time = 0;
-    updateTimerDisplay();
-}
-
-function updateTimer() {
-    time++;
-    updateTimerDisplay();
-}
-
-function updateTimerDisplay() {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
-
-    const formattedTime = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
-    document.getElementById('timer').innerText = formattedTime;
-}
-
-function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
+    document.getElementById('timer').querySelector('h1').textContent = '00:00:00';
 }
